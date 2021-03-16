2c7edaa636e6f0fd4351604600c9b4055e37117b
vasyillakafy <vasyillakafy@gmail.com>
2021-03-16 16:15:17 +0700

    Create c.java

---
+++ Minggu 2/c.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tugas;
+
+/**
+ *
+ * @author Administrator
+ */
+public class c {
+    public static void main(String[] args) {
+        
+        //benar
+        boolean gameOver = false;
+        int students = 50, classes = 3;
+        double sales_tax;
+        short number1;
+        
+        //salah
+        int 2beOrNot2be ; 
+            //tidak boleh menggunakan angka diawal variabel
+        float price index ; 
+            //tidak boleh menggunakan spasi pada variabel
+        double lastYear'sPrice ;
+            //tidak boleh menggunakan tanda petik satu pada variabel
+        long class ;
+            //variabel tidak boleh sama dengan keyword java
+        
+    }
+}
+
