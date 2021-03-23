947c2a61eca21b1071e278e5a9fd2f2751f93102
vasyillakafy <vasyillakafy@gmail.com>
2021-03-23 16:41:44 +0700

    Create LatihanB.java

---
+++ Minggu 2/LatihanB.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan;
+
+/**
+ *
+ * @author Administrator
+ */
+public class b {
+    public static void main(String[] args) {
+    
+    String Nama = "Indonesia" ;
+    char abjad = 'v';
+    int A = 7 ;
+    double B = 7.7 ;
+    
+    System.out.println (Nama) ;
+    System.out.println (B) ;
+    
+    }
+}
