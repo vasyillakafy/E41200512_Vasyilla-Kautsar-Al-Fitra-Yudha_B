011f81b3c7cbc9764d8144be0ba6da68b6a8f626
vasyillakafy <vasyillakafy@gmail.com>
2021-03-28 20:34:20 +0700

    Create Latihan1.java

---
+++ Minggu 3/Latihan1.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MINGGU_3;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Latihan1 {
+    public static void main(String[] args){
+    int [] angka = {1, 2, 3, 6, 7, 8, 9, 12, 15, 16};
+    
+    System.out.println("Jumlah elemen pada array angka = " + angka.length);
+    
+    }
+}
