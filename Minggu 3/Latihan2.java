9cb5c913abdfd7db9c66d8f3ad9a4dd4eab358a0
vasyillakafy <vasyillakafy@gmail.com>
2021-03-28 20:34:23 +0700

    Create Latihan2.java

---
+++ Minggu 3/Latihan2.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package MINGGU_3;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Latihan2 {
+    public static void main(String[] args) {
+        int [] angka = {1, 2, 3, 4, 5, 6};
+        String [] nama = {"Saya", "sedang", "belajar", "OOP", "Java"};
+        
+        for(int i=0; i<angka.length; i++){
+            System.out.println(angka[i]);
+    }
+        for(int i=0; i<nama.length; i++){
+            System.out.print(nama[i]);
+            System.out.print(" ");
+    }
+    }
+    
+    }
+
