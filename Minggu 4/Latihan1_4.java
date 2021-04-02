52e37a2fc2663d11defc1969b1bb8b1e83b7d153
vasyillakafy <vasyillakafy@gmail.com>
2021-04-02 14:51:02 +0700

    Create Latihan1_4.java

---
+++ Minggu 4/Latihan1_4.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package minggu4;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Latihan1_4 {
+    public static void main(String[] args) {
+        //STATEMENT WHILE
+        
+        int i =0;
+        
+        while ( i<= 10){
+            System.out.println("Perulangan ke - " + i);
+            i++;
+        }
+        
+              
+    }
+}
